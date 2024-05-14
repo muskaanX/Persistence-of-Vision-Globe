4321582b5e3df3aaf7114184be3b57307b92f688
muskaanX <79445340+muskaanX@users.noreply.github.com>
2024-05-15 04:59:38 +0530

    Update image_conversion.py

--- image_conversion.py
+++ image_conversion.py
@@ -1,20 +1,36 @@
 from PIL import Image
-import numpy as np
 
-def prepare_image(image_path, output_width, output_height):
-  img = Image.open(image_path)
-  img = img.resize((output_width, output_height))
-  img = img.convert('RGB')
+# Loading the image
+image_path = "globe_pov.jpg"
+image = Image.open(image_path)
 
-  data = []
-  for i in range(img.height):
-    row = []
-    for j in range(img.width):
-      pixel = img.getpixel((j, i))
-      color = (pixel[0] << 16) + (pixel[1] << 8) + pixel[2]
-      row.append(color)
-    data.append(row)
-  return np.array(data)
+# Obtaining the dimensions of the image
+original_width, original_height = image.size
 
-image_data = prepare_image('globe_pov.jpg', 360, 64)  # 360 degrees and 64 LEDs per side
-np.savetxt('image_data.csv', image_data, delimiter=',', fmt='%d') # save data as csv to be read further
+# Defining the desired height as 64 because we have 64 LEDs (feel free to modify as required)
+resize_height = 64
+
+# Calculating the dimensions for splitting
+split_width = original_width // 2
+
+# Splitting the image into left and right parts
+left_image = image.crop((0, 0, split_width, original_height))
+right_image = image.crop((split_width, 0, original_width, original_height))
+
+# Resizing the left part while maintaining the aspect ratio
+left_aspect_ratio = split_width / original_height
+left_resize_width = int(left_aspect_ratio * resize_height)
+left_image_resized = left_image.resize((left_resize_width, resize_height))
+
+# Resizing the right part while maintaining the aspect ratio
+right_aspect_ratio = (original_width - split_width) / original_height
+right_resize_width = int(right_aspect_ratio * resize_height)
+right_image_resized = right_image.resize((right_resize_width, resize_height))
+
+# Saving the resized images
+left_image_resized.save("left_image.jpg")
+right_image_resized.save("right_image.jpg")
+
+# If Needed: Displaying the resized images
+# left_image_resized.show()
+# right_image_resized.show()
